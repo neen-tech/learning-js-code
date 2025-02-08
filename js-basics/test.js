7e55a77b727bd8b3e98362c467bb3dc60373cf55
NitinKumarMck <nitin.kumar@mckesson.ca>
2025-02-08 19:19:55 +0530

    updated test file

--- js-basics/test.js
+++ js-basics/test.js
@@ -1,1 +1,43 @@
-console.log("Nitin Tandon")
+/*
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+
+
+// Number
+let length = 16;
+let weight = 7.5;
+
+//String
+let color = "YELLOW"
+let alpha = "Johnson"
+
+//Boolean
+let x = true
+let x2 = false
+
+//Object
+
+const person = {firstname: "John", lastname: "doe", email: "n@gmail.com"}
+
+//Array
+
+const car = []
+*/
+
+let score = 33
+
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+
+let dataNumber = "33abc"
+console.log(typeof (dataNumber))
+let numberdata = Number(dataNumber)
+console.log(numberdata)
